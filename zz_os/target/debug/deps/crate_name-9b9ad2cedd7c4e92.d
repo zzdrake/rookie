94fc35a3fb2b31e33b85fe0d9df116eb0af0b70f
zzdrake <1484093319@qq.com>
2024-08-09 11:32:45 +0800

    独立可执行程序

---
+++ zz_os/target/debug/deps/crate_name-9b9ad2cedd7c4e92.d
@@ -0,0 +1,5 @@
+/home/lvguanzhong/rookie/zz_os/target/debug/deps/libcrate_name-9b9ad2cedd7c4e92.rmeta: src/main.rs
+
+/home/lvguanzhong/rookie/zz_os/target/debug/deps/crate_name-9b9ad2cedd7c4e92.d: src/main.rs
+
+src/main.rs:
