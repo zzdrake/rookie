7a6093b37fd73593f401bb28de6ba0ad4736bfab
zzdrake <1484093319@qq.com>
2024-08-09 17:12:43 +0800

    4

--- zz_os/src/main.rs
+++ zz_os/src/main.rs
@@ -2,24 +2,17 @@
 #![no_main] // 禁用所有 Rust 层级的入口点
 
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    print!("Hello World{}", "!");
     loop {}
 }
 
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
--- zz_os/target/x86_64-zz_os/debug/deps/zz_os-c7ac7da49080ae07.d
+++
@@ -1,5 +0,0 @@
-/home/lvguanzhong/rookie/zz_os/target/x86_64-zz_os/debug/deps/libzz_os-c7ac7da49080ae07.rmeta: src/main.rs
-
-/home/lvguanzhong/rookie/zz_os/target/x86_64-zz_os/debug/deps/zz_os-c7ac7da49080ae07.d: src/main.rs
-
-src/main.rs:
