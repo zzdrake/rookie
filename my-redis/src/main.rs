43f8a2a337825806e195b69f28fc77bf9b6e3a27
zzdrake <1484093319@qq.com>
2024-09-04 10:29:41 +0800

    3

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,43 +1,43 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use std::{collections::HashMap, sync::{Arc, Mutex}};
+use bytes::Bytes;
 
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 #[tokio::main]
 async fn main() {
     // 监听指定端口ip， 等待TCP连接
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // 第二个被忽略的项中含有新连接的IP 和端口信息
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
+
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
 
-    // 使用 hashmap 来存储 redis 的数据
-    let mut db = HashMap::new();
-
-    // `mini-redis` 提供的便利函数，使用返回的 `connection` 可以用于从 socket 中读取数据并解析为数据帧
     let mut connection = Connection::new(socket);
 
-    // 使用 `read_frame` 方法从连接获取一个数据帧：一条redis命令 + 相应的数据
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // 值被存储为 `Vec<u8>` 的形式
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    // `Frame::Bulk` 期待数据的类型是 `Bytes`， 该类型会在后面章节讲解，
-                    // 此时，你只要知道 `&Vec<u8>` 可以使用 `into()` 方法转换成 `Bytes` 类型
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
@@ -45,7 +45,6 @@ async fn process(socket: TcpStream) {
             cmd => panic!("unimplemented {:?}", cmd),
         };
 
-        // 将请求响应返回给客户端
         connection.write_frame(&response).await.unwrap();
     }
 }
